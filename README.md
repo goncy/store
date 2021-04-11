1e128e92613bb0c158586090095c6f1d35c268da
goncy <gonzalo.pozzo4@gmail.com>
2021-04-11 11:11:53 -0300

    Update copyright

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Store
+# Almacency
+Tienda online administrada via Google Sheets.
 
 # ¿Qué puedo hacer con este repositorio?
 Podés leer la licencia [acá](./LICENSE.md). En resumen, podés usar este repositorio para lo que quieras mientras no lucres con eso y menciones la fuente original cuando lo uses 🥰.
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import Head from "next/head";
 import {
   ChakraProvider,
   Heading,
@@ -8,6 +9,7 @@ import {
   VStack,
   Box,
   Divider,
+  Link,
 } from "@chakra-ui/react";
 import {AppProps} from "next/app";
 
@@ -15,25 +17,44 @@ import theme from "../theme";
 
 const App: React.FC<AppProps> = ({Component, pageProps}) => {
   return (
-    <ChakraProvider theme={theme}>
-      <Box padding={4}>
-        <Container
-          backgroundColor="white"
-          borderRadius="sm"
-          boxShadow="md"
-          maxWidth="container.xl"
-          padding={4}
-        >
-          <VStack marginBottom={6}>
-            <Image borderRadius={9999} src="//placehold.it/128x128" />
-            <Heading>Almacency</Heading>
-            <Text>El almacen de Goncy</Text>
-          </VStack>
-          <Divider marginY={6} />
-          <Component {...pageProps} />
-        </Container>
-      </Box>
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Mi tienda online - Almacency</title>
+        <meta content="initial-scale=1.0, width=device-width" name="viewport" />
+        {/* Inicio de meta tags de licencia - Cambiar el contenido de los mismos viola el contenido de los terminos de licencia */}
+        <meta content="goncy" name="author" />
+        <meta content="Gonzalo Pozzo" name="copyright" />
+        {/* Fin de meta tags de licencia */}
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Box padding={4}>
+          <Container
+            backgroundColor="white"
+            borderRadius="sm"
+            boxShadow="md"
+            maxWidth="container.xl"
+            padding={4}
+          >
+            <VStack marginBottom={4}>
+              <Image borderRadius={9999} src="//placehold.it/128x128" />
+              <Heading>Almacency</Heading>
+              <Text>El almacen de Goncy</Text>
+            </VStack>
+            <Divider marginY={4} />
+            <Component {...pageProps} />
+            <Divider marginY={4} />
+            {/* Inicio de copyright - Cambiar el contenido de los mismos viola el contenido de los terminos de licencia */}
+            <Text textAlign="center">
+              © Copyright {new Date().getFullYear()}. Hecho con ♥ para la comunidad, por{" "}
+              <Link isExternal href="https://gonzalopozzo.com">
+                goncy
+              </Link>
+            </Text>
+            {/* Fin de copyright */}
+          </Container>
+        </Box>
+      </ChakraProvider>
+    </>
   );
 };
 
