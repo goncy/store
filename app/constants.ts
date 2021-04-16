4640e5210252e76f862665ce7f1dc342541316fe
goncy <gonzalo.pozzo4@gmail.com>
2021-04-16 16:33:29 -0300

    Update constants

--- app/constants.ts
+++ app/constants.ts
@@ -1,11 +1,11 @@
 export const INFORMATION = {
-  avatar: "//placehold.it/256x256",
-  banner: "//placehold.it/1440x320",
+  avatar: "/banner.avatar",
+  banner: "/banner.jpg",
   title: "URBN Catering",
   description: "Galardonada pizza estilo New Haven en Palermo SOHO.",
   phone: `5491141414141`,
   sheet: `https://docs.google.com/spreadsheets/d/e/2PACX-1vSk6SRbveAHR5DVL_DoFCMXNNMV37BD-J-GzY9i8u246Gwiv5MstNkRSRi2VJNln3W5PaIduadgiRg2/pub?output=csv`,
-  color: `orange`,
+  color: `teal`,
   social: [
     {
       name: "instagram",
