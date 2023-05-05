c9922910c3b5d60ffab318deb723f7ac63710cfe
goncy <gonzalo.pozzo4@gmail.com>
2023-05-05 14:28:19 -0300

    Use revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,7 +1,7 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.headers["x-secret"] === process.env.REVALIDATE_SECRET) {
+  if (req.query.secret === process.env.REVALIDATE_SECRET) {
     await res.revalidate("/");
 
     return res.json({revalidate: true});
