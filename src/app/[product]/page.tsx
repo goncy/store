d41b8706c65a2d60ca91ece6fbd75051dcc0d9a0
goncy <gonzalo.pozzo4@gmail.com>
2023-08-22 15:31:28 -0300

    Generate static pages

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -2,6 +2,12 @@ import api from "~/product/api";
 
 import ProductPageClient from "./client";
 
+export async function generateStaticParams() {
+  const products = await api.list();
+
+  return products.map((product) => ({product: product.id}));
+}
+
 const ProductPage = async ({params: {product}}: {params: {product: string}}) => {
   const data = await api.fetch(product);
 
