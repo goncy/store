649e8592ca35b9d4e2366d2ffebc8cf34ef38395
goncy <gonzalo.pozzo4@gmail.com>
2023-08-22 15:33:22 -0300

    Generate metadata

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -1,3 +1,5 @@
+import type {Metadata} from "next";
+
 import api from "~/product/api";
 
 import ProductPageClient from "./client";
@@ -6,6 +8,19 @@ export async function generateStaticParams() {
   const products = await api.list();
 
   return products.map((product) => ({product: product.id}));
+}
+
+export async function generateMetadata({
+  params: {product: productId},
+}: {
+  params: {product: string};
+}): Promise<Metadata> {
+  const product = await api.fetch(productId);
+
+  return {
+    title: product.title,
+    description: product.description,
+  };
 }
 
 const ProductPage = async ({params: {product}}: {params: {product: string}}) => {
