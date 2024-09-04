e08c4ada21a3b06a1b60b5111fac1a64f7aeb15c
goncy <gonzalo.pozzo4@gmail.com>
2024-09-04 14:25:24 -0300

    Update stale times

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    staleTimes: {
+      static: 900,
+      dynamic: 0,
+    },
+  },
 };
